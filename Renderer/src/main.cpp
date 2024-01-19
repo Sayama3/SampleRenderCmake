329e0872af28b69ee03449acff5cf6a86bfe0e8b
Iannis Poquet <ianpo@live.fr>
2024-01-19 15:40:06 +0100

    fix macos initialization

--- Renderer/src/main.cpp
+++ Renderer/src/main.cpp
@@ -15,29 +15,12 @@ int main(int argc, char** argv)
 		throw std::runtime_error("Couldn't initialize GLFW.");
 
 	// Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-	// GL ES 2.0 + GLSL 100
-        glsl_version = "#version 100";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-        glfwWindowHint(GLFW_RESIZABLE, allowResize ? GL_TRUE : GL_FALSE);
-#elif defined(__APPLE__)
-	// GL 3.2 + GLSL 150
-        glsl_version = "#version 150";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-        glfwWindowHint(GLFW_RESIZABLE, allowResize ? GL_TRUE : GL_FALSE);
-#else
 	// GL 4.6 + GLSL 330
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-#endif
 
 	/* Create a windowed mode window and its OpenGL context */
 	auto m_Window = glfwCreateWindow(1600, 900, "Renderer", nullptr, nullptr);
